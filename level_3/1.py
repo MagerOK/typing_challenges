8e08588320a4bebcdf96f7ee487f46e5448818b0
MagerOK <farmarker@gmail.com>
2023-05-31 14:35:06 +0200

    completed 3rd level

--- level_3/1.py
+++ level_3/1.py
@@ -1,9 +1,12 @@
 import decimal
 
 from constants import ___
+from typing import Mapping, Optional
 
 
-def get_transaction_amount(transaction_id: ___, transactions_amounts_map: ___) -> ___:
+def get_transaction_amount(transaction_id: int,
+                           transactions_amounts_map: Mapping[int, decimal.Decimal],
+                           ) -> Optional[decimal.Decimal]:
     # попробуйте использовать typing.Mapping: transactions_amounts_map по смыслу не должен меняться внутри функции
     pass
 
